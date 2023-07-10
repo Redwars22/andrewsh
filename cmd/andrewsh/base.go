1c53bc3696392fd32931410a21861a04fbceceb5
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-07-10 17:17:48 -0300

    adicionando suporte a mais comandos

--- cmd/andrewsh/base.go
+++ cmd/andrewsh/base.go
@@ -11,15 +11,39 @@ import (
 func readAndInterpret() {
 	reader := bufio.NewReader(os.Stdin)
 
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSuffix(input, "\n")
-	args := strings.Split(input, " ")
-	cmd := exec.Command(args[0], args[1:]...)
-	output, err := cmd.Output()
+	for {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input, "\n")
+		args := strings.Split(input, " ")
 
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println(string(output))
+		if len(args) == 0 {
+			continue
+		}
+
+		cmd := exec.Command(args[0], args[1:]...)
+
+		switch args[0] {
+		case "exit":
+			return
+		case "cd":
+			if len(args) < 2 {
+				fmt.Println("Error: Directory argument is missing.")
+				continue
+			}
+			err := os.Chdir(args[1])
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+			return
+		default:
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+		}
 	}
 }
